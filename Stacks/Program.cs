afff5d1841fe388d03afa6040fb018b4fb1c9d26
Paloftzer <phille04@outlook.com>
2024-09-03 15:39:29 +0200

    Added a Stack folder with examples of stacks

---
+++ Stacks/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Stacks;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<string> vehicles = new();
+        vehicles.Push("Volvo");
+        vehicles.Push("Ford");
+        vehicles.Push("KIA");
+        vehicles.Push("Fiat");
+        vehicles.Push("Tesla");
+
+        while(vehicles.Count > 0)
+        {
+            Console.WriteLine($"Manufacturer: {vehicles.Pop()}");
+        }
+    }
+}
