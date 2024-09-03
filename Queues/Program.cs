c0ef2c8083a8facad21a5af2f438d98893049ae5
Paloftzer <phille04@outlook.com>
2024-09-03 15:16:57 +0200

    Added a Queue folder with examples of queues

---
+++ Queues/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Queues;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> vehicles = [];
+        vehicles.Enqueue("Volvo");
+        vehicles.Enqueue("Ford");
+        vehicles.Enqueue("KIA");
+        vehicles.Enqueue("Fiat");
+        vehicles.Enqueue("Tesla");
+
+        while (vehicles.Count > 0)
+        {
+            Console.WriteLine($"Manufacturer: {vehicles.Dequeue()}");
+        }
+    }
+}
