4c47804a7cdef77d2576c02bf53819ebeb3a0c53
Paloftzer <phille04@outlook.com>
2024-09-04 15:15:27 +0200

    Small fix

--- Banking/Program.cs
+++ Banking/Program.cs
@@ -7,8 +7,8 @@ class Program
     {
         BankAccount myAccount = new();
         myAccount.AccountNumber = "ABC123";
-        // Console.WriteLine($"Your account number is: {myAccount.AccountNumber()}");
-        Console.WriteLine("Your account number is: {0}, the date of creation is: {1}", myAccount.AccountNumber, myAccount.CreatedAt);
+        Console.WriteLine($"Your account number is: {myAccount.AccountNumber}, the date of creation is: {myAccount.CreatedAt}");
+        // Console.WriteLine("Your account number is: {0}, the date of creation is: {1}", myAccount.AccountNumber, myAccount.CreatedAt);
 
         BankAccount secondAccount = new("XYZ789");
         BankAccount thirdAccount = new(500,"XYZ789");
