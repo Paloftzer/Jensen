2451f16396dda6e98bc64a7e66a732bb49701364
Paloftzer <phille04@outlook.com>
2024-09-24 15:56:11 +0200

    Added an invoice controller

---
+++ Lesson-11/Westcoast-Shop/Controllers/InvoiceController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Controllers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
